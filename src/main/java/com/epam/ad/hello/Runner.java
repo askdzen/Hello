282b95a6cd66bf73c3defe4af3525c1c2d541948
askdzen <ask.dzen@mail.ru>
2014-06-06 08:57:21 +0600

    Add World

--- src/main/java/com/epam/ad/hello/Runner.java
+++ src/main/java/com/epam/ad/hello/Runner.java
@@ -3,7 +3,7 @@ public class Runner {
 
     public static void main(String[] args) {
         Greeter g = new Greeter();
-        g.setGreeter("Hello John!");
+        g.setGreeter("Hello World!");
         System.out.println(g.getGreeter());
 
     }
